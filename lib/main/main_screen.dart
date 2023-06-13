de26302a5da2f66d555c4ea4892b2073a5b67056
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-14 01:17:42 +0900

    058. SharedPreference 로 간단한 값 저장

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_calculator/result/result_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -10,6 +11,23 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    save();
+
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  Future save() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('height', double.parse(_heightController.text));
+    await prefs.setDouble('weight', double.parse(_weightController.text));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +43,7 @@ class _MainScreenState extends State<MainScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               TextFormField(
+                controller: _heightController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '키',
@@ -39,6 +58,7 @@ class _MainScreenState extends State<MainScreen> {
               ),
               const SizedBox(height: 8),
               TextFormField(
+                controller: _weightController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '몸무게',
@@ -54,15 +74,16 @@ class _MainScreenState extends State<MainScreen> {
               const SizedBox(height: 8),
               ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
+                  if (_formKey.currentState?.validate() == false) {
                     return;
                   }
+
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const ResultScreen(
-                        height: 180,
-                        weight: 77.5,
+                      builder: (context) => ResultScreen(
+                        height: double.parse(_heightController.text),
+                        weight: double.parse(_weightController.text),
                       ),
                     ),
                   );

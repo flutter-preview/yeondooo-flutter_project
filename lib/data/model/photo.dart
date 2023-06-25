c09e9faf689c6842755bcf982c1b3b61e582c8e4
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-25 18:54:37 +0900

    feat: photo model 구현

---
+++ lib/data/model/photo.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+}

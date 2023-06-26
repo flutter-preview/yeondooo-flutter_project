1ce9f9e6c043aea18830315787d82999164714ba
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-26 23:33:16 +0900

    feat: AccountPage UI 작성

--- lib/tab/account/account_page.dart
+++ lib/tab/account/account_page.dart
@@ -5,6 +5,110 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instagram Clone'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://cdn.mmnews.co.kr/news/photo/202204/8717_7554_2424.jpg'),
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      alignment: Alignment.bottomRight,
+                      child: const Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            width: 28,
+                            height: 28,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  '아이유',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '3',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '게시물',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '팔로워',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '팔로잉',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

90ca98f5a273ae6daacd5893362ff83c0937d4fa
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-26 23:16:06 +0900

    feat: HomePage UI 작성

--- lib/tab/home/home_page.dart
+++ lib/tab/home/home_page.dart
@@ -9,10 +9,78 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Instagram Clone'),
       ),
-      body: const Column(
-        children: [
-          Text('Instagram 에 오신 것을 환영 합니다.'),
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              'Instagram 에 오신 것을 환영 합니다.',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text('사진과 동영상을 보려면 팔로우 하세요.'),
+            const SizedBox(height: 20),
+            Card(
+              elevation: 4.0,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://cdn.mmnews.co.kr/news/photo/202204/8717_7554_2424.jpg'),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('test@test.com'),
+                    const SizedBox(height: 8),
+                    const Text('닉네임'),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          'https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/pt/2022/06/28/202206281630779508_62bab09b47ba0.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 4),
+                        Image.network(
+                          'https://cphoto.asiae.co.kr/listimglink/1/2022071515463751809_1657867597.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 4),
+                        Image.network(
+                          'https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2023/04/27/0e83f60f-51d4-49b2-919c-92ec2ff928c5.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Facebook 친구'),
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('팔로우'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

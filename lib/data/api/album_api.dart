d4684d8a2a9b73d471e76065f203c21dc240e053
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-28 04:27:19 +0900

    feat: album api 구현

---
+++ lib/data/api/album_api.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../dto/album_dto.dart';
+
+class AlbumApi {
+  Future<List<AlbumDto>> getAlbums() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+
+    final json = jsonDecode(response.body) as List<dynamic>;
+    final fromJson = json.map((e) => AlbumDto.fromJson(e)).toList();
+
+    return fromJson;
+  }
+}

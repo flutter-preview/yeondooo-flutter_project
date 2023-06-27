a08eb45c671e1a79e60bb3b7f7a1771ec590d31a
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-28 05:06:43 +0900

    feat: photo dto 구현

---
+++ lib/data/dto/photo_dto.dart
@@ -0,0 +1,37 @@
+class PhotoDto {
+  PhotoDto({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+  late final int albumId;
+  late final int id;
+  late final String title;
+  late final String url;
+  late final String thumbnailUrl;
+
+  PhotoDto.fromJson(Map<String, dynamic> json){
+    albumId = json['albumId'];
+    id = json['id'];
+    title = json['title'];
+    url = json['url'];
+    thumbnailUrl = json['thumbnailUrl'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['albumId'] = albumId;
+    _data['id'] = id;
+    _data['title'] = title;
+    _data['url'] = url;
+    _data['thumbnailUrl'] = thumbnailUrl;
+    return _data;
+  }
+
+  @override
+  String toString() {
+    return 'PhotoDto{albumId: $albumId, id: $id, title: $title, url: $url, thumbnailUrl: $thumbnailUrl}';
+  }
+}
